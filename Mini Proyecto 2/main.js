a9df7fd75491c9e8263becf9f00cac442ef919d3
Pabel Hernandez <pabel.hernandez@live.com>
2023-06-08 02:55:45 -0600

    updates

--- Mini Proyecto 2/main.js
+++ Mini Proyecto 2/main.js
@@ -1,11 +1,11 @@
 const container = document.querySelector('.container');
-let counter = 3;
+let counter = 4;
 
 window.addEventListener('scroll', () => {
   const { scrollHeight, clientHeight, scrollTop } = document.documentElement;
 
   console.log(`scrollTop + clientHeight = ${scrollTop + clientHeight} | Altura personalizada = ${scrollHeight - 3}`);
-  scrollTop + clientHeight > scrollHeight - 3 && setTimeout(newContainer, 2000);
+  scrollTop + clientHeight > scrollHeight - 3 && setTimeout(newContainer, 500);
 });
 
 const newContainer = () => {
