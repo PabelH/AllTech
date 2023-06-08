26b4f8c6e06488332566222a2cf252343d6cbdf6
Pabel Hernandez <pabel.hernandez@live.com>
2023-06-08 02:48:13 -0600

    update MP2

---
+++ Mini Proyecto 2/main.js
@@ -0,0 +1,21 @@
+const container = document.querySelector('.container');
+let counter = 3;
+
+window.addEventListener('scroll', () => {
+  const { scrollHeight, clientHeight, scrollTop } = document.documentElement;
+
+  console.log(`scrollTop + clientHeight = ${scrollTop + clientHeight} | Altura personalizada = ${scrollHeight - 3}`);
+  scrollTop + clientHeight > scrollHeight - 3 && setTimeout(newContainer, 2000);
+});
+
+const newContainer = () => {
+  const box = document.createElement('div');
+  box.className = 'box d';
+
+  const text = document.createElement('span');
+  text.textContent = `Elemento ${counter++}`;
+  text.className = 'overlay-text';
+  box.appendChild(text);
+
+  container.appendChild(box);
+};
