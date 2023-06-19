b9f934dce63cd61613bd2efcdca8c325f0d4f875
Pabel Hernandez <pabel.hernandez@live.com>
2023-06-19 10:19:37 -0600

    nav bar + about us

--- Python+JS/app.py
+++ Python+JS/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about', strict_slashes=False)
+def about():
+    return render_template("about.html")
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
